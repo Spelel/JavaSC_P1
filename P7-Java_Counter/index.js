9cac289e903525e71d0d31d906c4c6bdcdbaec46
Arek <arek-karczo.wp.pl@wp.pl>
2024-08-19 20:05:01 +0200

    . . .

--- P7-Java_Counter/index.js
+++ P7-Java_Counter/index.js
@@ -2,21 +2,76 @@ let countEl = document.getElementById("count-el")
 
 let count = 0
 
+let saveEl = document.getElementById("save-el")
+
 function increment() {
-    count = count + 1 
-    countEl.innerText = count
+    count += 1 
+    countEl.textContent = count
     // console.log(count)
 }
 
 
 function save() {
-    console.log(count)
+    let coun = count + " - "
+    saveEl.textContent += coun
+    // console.log(count)
+    countEl.textContent = 0
+    count = 0
 }
 
 
 
-let name = "Arek"
-let greetings = "Hi, my name is "
-let myGreetings = greetings + name
+let myPoints = 3
 
-console.log(myGreetings)
+function add3Points() {
+    myPoints  += 3
+    console.log(myPoints)
+}
+
+function remove1Point() {
+    myPoints -= 1
+    console.log(myPoints)
+}
+
+
+
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+// let name = "Arek"
+// let greetings = "Hi "
+
+// welcomeEl.innerText = greetings + name
+
+
+// welcomeEl.innerText = welcomeEl.innerText + "👋"
